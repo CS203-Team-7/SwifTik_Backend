a7e443f59fd361bc31c40be6f9c64a63f5e479ca
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-08-28 21:58:47 +0800

    Adding base files

---
+++ swiftyticketing/src/main/java/com/swiftyticket/SwiftyticketingApplication.java
@@ -0,0 +1,13 @@
+package com.swiftyticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftyticketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftyticketingApplication.class, args);
+	}
+
+}
