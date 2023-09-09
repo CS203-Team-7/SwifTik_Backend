5c5ca326f2cbe839593561b9745fdd82e4a0e001
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-09-09 17:13:16 +0800

    Successfully connected to DB

--- swiftyticketing/src/main/java/com/swiftyticket/models/User.java
+++ swiftyticketing/src/main/java/com/swiftyticket/models/User.java
@@ -3,20 +3,24 @@ package com.swiftyticket.models;
 import java.util.Date;
 // import java.util.Set;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
     private Integer userId;
     private String email;
     private String password;
