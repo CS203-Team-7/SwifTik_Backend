14d506f31a03df4fda1aff8d733ac6e43e31ddb1
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-09-10 16:58:25 +0800

    Created Dto classes for a signIn Request and a SignUpRequest to enforce cleaner code

---
+++ swiftyticketing/src/main/java/com/swiftyticket/dto/auth/SignUpRequest.java
@@ -0,0 +1,19 @@
+package com.swiftyticket.dto.auth;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequest {
+    // This is just to format the data that is sent when a new user is signing up in the website:
+    private String email;
+    private String password;
+    private Date dateOfBirth;
+}
