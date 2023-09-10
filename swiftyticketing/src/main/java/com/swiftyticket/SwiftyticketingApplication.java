bc4b01b41522b40f8a0fcd770575b3796395587d
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-09-10 17:20:42 +0800

    Testing

--- swiftyticketing/src/main/java/com/swiftyticket/SwiftyticketingApplication.java
+++ swiftyticketing/src/main/java/com/swiftyticket/SwiftyticketingApplication.java
@@ -2,12 +2,22 @@ package com.swiftyticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class SwiftyticketingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwiftyticketingApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "Hello there user";
 	}
 
 }
