4848b16707281e32e6900ae17a11cfb5eb209c22
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-09-10 16:19:47 +0800

    Added security dependencies and changed User model to implement UserDetails

---
+++ swiftyticketing/src/main/java/com/swiftyticket/models/Role.java
@@ -0,0 +1,6 @@
+package com.swiftyticket.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- swiftyticketing/src/main/java/com/swiftyticket/models/User.java
+++ swiftyticketing/src/main/java/com/swiftyticket/models/User.java
@@ -1,34 +1,74 @@
 package com.swiftyticket.models;
 
+import java.util.Collection;
 import java.util.Date;
 // import java.util.Set;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private Integer userId;
+    @Column(unique = true)
     private String email;
     private String password;
     private Date dateOfBirth;
     private String phoneNumber;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-    // Roles to set for security:
-    // private Set<Role> roles;
+    @Override
+    public String getUsername() {
+        return email;
+    }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
