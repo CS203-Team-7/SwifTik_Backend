cd1fa32538a9ddd0022a9667316908de3048b5cb
Grace Bisawan <gbisawan.2022@smu.edu.sg>
2023-09-12 13:59:12 +0800

    Implemented a GET, POST, PUT, and DELETE method for UserService and UserServiceImpl

--- swiftyticketing/src/main/java/com/swiftyticket/services/UserService.java
+++ swiftyticketing/src/main/java/com/swiftyticket/services/UserService.java
@@ -1,7 +1,18 @@
 package com.swiftyticket.services;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.swiftyticket.models.User;
+
 public interface UserService {
+    List<User> listUsers();
+    User getUser(Integer userID);
+    User getUserByEmail(String email);
+    User addUser(User user);
+    User updateUser(Integer userID, User user);
+    User updateUser(String email, User user);
+    void deleteUser(Integer userID);
     UserDetailsService userDetailsService();
 }
--- swiftyticketing/src/main/java/com/swiftyticket/services/implementations/UserServiceImpl.java
+++ swiftyticketing/src/main/java/com/swiftyticket/services/implementations/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.swiftyticket.services.implementations;
 
+import java.util.List;
+
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.swiftyticket.models.User;
 import com.swiftyticket.repositories.UserRepository;
 import com.swiftyticket.services.UserService;
 
@@ -16,6 +20,57 @@ public class UserServiceImpl implements UserService {
 
     // We are going to need the repository methods here:
     private final UserRepository userRepository;
+
+    @Override
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUser(Integer userID) {
+        return userRepository.findById(userID).map(user -> {
+            return user;
+        }).orElse(null);
+    }
+
+    @Override
+    public User getUserByEmail(String email){
+        return userRepository.findByEmail(email).map(user -> {
+            return user;
+        }).orElse(null);
+    }
+
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Integer userID, User newUserInfo) {
+        return userRepository.findById(userID).map(user -> {
+            user.setAge(newUserInfo.getAge());
+            user.setEmail(newUserInfo.getEmail());
+            user.setPassword(newUserInfo.getPassword());
+            user.setPhoneNumber(newUserInfo.getPhoneNumber());
+            return userRepository.save(user);
+        }).orElse(null);
+    }
+
+    @Override
+    public User updateUser(String email, User newUserInfo) {
+        return userRepository.findByEmail(email).map(user -> {
+            user.setAge(newUserInfo.getAge());
+            user.setEmail(newUserInfo.getEmail());
+            user.setPassword(newUserInfo.getPassword());
+            user.setPhoneNumber(newUserInfo.getPhoneNumber());
+            return userRepository.save(user);
+        }).orElse(null);
+    }
+
+    @Override
+    public void deleteUser(Integer userID){
+        userRepository.deleteById(userID);
+    }
 
     // Also implement the UserDetailsService for Spring security:
     @Override
