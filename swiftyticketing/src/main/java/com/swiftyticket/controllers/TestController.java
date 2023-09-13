3d41e9fef76dac3571f38c8d8938bd172177216e
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-09-14 00:25:14 +0800

    Testing a few things don't mind

--- swiftyticketing/src/main/java/com/swiftyticket/controllers/TestController.java
+++ swiftyticketing/src/main/java/com/swiftyticket/controllers/TestController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("test")
-public class testController {
+@RequestMapping("/test")
+public class TestController {
     @GetMapping
     public String testGet(){
-        return "yipeee";
+        return "User access level";
     }
     
 }
