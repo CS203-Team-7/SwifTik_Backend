a37d2f40ce58e1e9a1eca4c2703840c7732c150b
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-10-21 17:17:59 +0800

    Upgraded the JWT util service to implement smoother authentication

--- swiftyticketing/src/main/java/com/swiftyticket/services/implementations/JwtServiceImpl.java
+++ swiftyticketing/src/main/java/com/swiftyticket/services/implementations/JwtServiceImpl.java
@@ -1,20 +1,23 @@
 package com.swiftyticket.services.implementations;
 
+import com.swiftyticket.models.User;
 import com.swiftyticket.services.JwtService;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.logging.Logger;
 
 @Service
 public class JwtServiceImpl implements JwtService {
@@ -23,52 +26,65 @@ public class JwtServiceImpl implements JwtService {
     @Value("${token.signing.key}")
     private String jwtSigningKey;
 
-    // This will create the SHA key for the token using our signing key from properties file:
-    private Key getSigningKey(){
+    // We make a custom final string for issuer of the token:
+    private final static String ISSUER = "SwifTik.com";
+    // Logger for debugging:
+    private final Logger logger = Logger.getLogger(JwtServiceImpl.class.getName());
+
+    // Method to extract the key from the signing key:
+    private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    @Override
-    public String extractUserName(String token){
-        // To actually get to the username/email from the raw token we need to extract the claims:
-        return extractClaim(token, Claims::getSubject);
+    public String generateToken(User user) {
+        JwtBuilder token = Jwts.builder()
+                .setSubject(user.getEmail())
+                .setIssuer(ISSUER)
+                .setIssuedAt(new Date())
+                .setHeaderParam("user_id", user.getUserId())
+                .setHeaderParam("role", user.getRole())
+                .setExpiration(Date.from(LocalDate.now().plusDays(1).atStartOfDay().toInstant(java.time.ZoneOffset.UTC)))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256);
+        return token.compact();
     }
 
-    // This function is to extract a particular claim from all claims:
-    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
-        // This is to specify which particular claims are wanted:
-        final Claims claims = extractAllClaims(token);
-        return claimsResolvers.apply(claims);
-    }
-
-    // From the raw jwt token we need to extract all the claims from it by using the signing key:
-    private Claims extractAllClaims(String token){
-        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token)
+    public String extractUserName(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
                 .getBody();
+        return claims.getSubject();
     }
 
-    @Override
-    public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
+    public LocalDateTime getExpirationDate(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getExpiration().toInstant().atZone(java.time.ZoneOffset.UTC).toLocalDateTime();
     }
 
-    // This method is to actually create a fresh token:
-    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername()) // Set any extra claims and set the subject as the user's email
-                .setIssuedAt(new Date(System.currentTimeMillis())) // This is the time it was created
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 600)) // One day expiration limit for now:
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact(); // This is to encrypt the token with the key we are using
-    }
-
-    // This is to check expiration date: so we get the expiration from the claims:
-    @Override
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        return extractExpiration(token).after(new Date());
-    }
-
-    // Function to actually extract the expiration date from the token:
-    private Date extractExpiration(String token){
-        return extractClaim(token, Claims::getExpiration);
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch(SignatureException e) {
+            logger.info("Invalid JWT signature: " + e.getMessage());
+        } catch(MalformedJwtException e) {
+            logger.info("Invalid JWT token: " + e.getMessage());
+        } catch(ExpiredJwtException e) {
+            logger.info("JWT token is expired: " + e.getMessage());
+        } catch(UnsupportedJwtException e) {
+            logger.info("JWT token is unsupported: " + e.getMessage());
+        } catch(IllegalArgumentException e) {
+            logger.info("JWT claims string is empty: " + e.getMessage());
+        }
+        return false;
     }
 }
