845ef0b4dffc3e50712e03f9a8985aa112b9dc41
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-10-21 17:36:33 +0800

    Enhanced the Filter for security purposes

--- swiftyticketing/src/main/java/com/swiftyticket/config/JwtAuthFilter.java
+++ swiftyticketing/src/main/java/com/swiftyticket/config/JwtAuthFilter.java
@@ -1,6 +1,7 @@
 package com.swiftyticket.config;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
@@ -29,46 +30,48 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserService userService;
+    // We need another logger here:
+    private final Logger logger = Logger.getLogger(JwtAuthFilter.class.getName());
 
     // Below is the filter chain we make that uses jwt authentication to allow users or not:
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-        
-                // First of all we need to get the token from the request:
-                final String authHeader = request.getHeader("Authorization");
-                String jwtToken;
-                final String userEmail;
-                // Here we have to check if the there is no authorization token or if it isn't a jwt token (JWT Tokens start with Bearer ):
-                // If it is true, then there is no filtering necessary from the JWT:
-                if(authHeader == null || !authHeader.startsWith("Bearer ")){
-                    filterChain.doFilter(request, response);
-                    return;
-                }
-                // Here we have to begin the process of validating the token:
-                jwtToken = authHeader.substring(7);
-                userEmail = jwtService.extractUserName(jwtToken); // This is the method we implemented in the jwtServiceImpl
-                // As long as the email isn't empty and the authentication of that email is ok that means there is a user email associated to the token:
-                if(!userEmail.isEmpty() && SecurityContextHolder.getContext().getAuthentication() == null){
-                    // We fill in the user details into the spring security class of UserDetail
-                    UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
-                    // Now we check if the token itself is valid or not:
-                    if(jwtService.isTokenValid(jwtToken, userDetails)){
-                        // Then we go ahead and authorize:
-                        SecurityContext context = SecurityContextHolder.createEmptyContext();
-                        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken
-                                                                        (userDetails, null, userDetails.getAuthorities());
-                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                        context.setAuthentication(authToken);
-                        SecurityContextHolder.setContext(context);
-                        log.info("" + context);
-                    } else {
-                        throw new JWTExpiredException(userEmail + " token has expired, Please login again");
-                    }
-                }
-                // Then finally we go back to the security filterchain since it has been authorized here:
-                filterChain.doFilter(request, response);
 
+        // We need to get the token from the request header and validate it:
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null) {
+            logger.warning("No Authorization header found");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        if (!authHeader.startsWith("Bearer ")) {
+            logger.warning("Invalid Authorization header found");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // We need to extract the token from the header and validate it:
+        final String token = authHeader.substring(7);
+
+        if (!jwtService.isTokenValid(token)) {
+            logger.warning("Invalid token found");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Get user details from the token:
+        UserDetails userDetails = userService.userDetailsService().loadUserByUsername(jwtService.extractUserName(token));
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null,
+                userDetails == null ? null : userDetails.getAuthorities()
+        );
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        // We need to set the security context to the authentication:
+        logger.info("Auth OK");
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
     }
-    
 }
