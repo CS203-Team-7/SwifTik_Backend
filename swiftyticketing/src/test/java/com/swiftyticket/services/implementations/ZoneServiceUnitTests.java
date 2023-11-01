9bf84c64d9c6040c5af2da6cb9e00b18beb07083
Grace Bisawan <gbisawan.2022@smu.edu.sg>
2023-11-02 01:52:34 +0800

    Created unit tests for joinRaffle event not found and user not found

--- swiftyticketing/src/test/java/com/swiftyticket/services/implementations/ZoneServiceUnitTests.java
+++ swiftyticketing/src/test/java/com/swiftyticket/services/implementations/ZoneServiceUnitTests.java
@@ -1,33 +1,18 @@
 package com.swiftyticket.services.implementations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.anyBoolean;
-import static org.mockito.Mockito.anyDouble;
-import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.swiftyticket.dto.zone.ZoneRequest;
-import com.swiftyticket.exceptions.EventNotFoundException;
-import com.swiftyticket.exceptions.WrongZoneDateException;
-import com.swiftyticket.models.Event;
-import com.swiftyticket.models.Role;
-import com.swiftyticket.models.Ticket;
-import com.swiftyticket.models.User;
-import com.swiftyticket.models.Zones;
-import com.swiftyticket.repositories.EventRepository;
-import com.swiftyticket.repositories.UserRepository;
-import com.swiftyticket.repositories.ZoneRepository;
-
 import java.time.LocalDate;
 import java.time.ZoneOffset;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -40,6 +25,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.swiftyticket.dto.zone.ZoneRequest;
+import com.swiftyticket.exceptions.EventNotFoundException;
+import com.swiftyticket.exceptions.UserNotFoundException;
+import com.swiftyticket.exceptions.WrongZoneDateException;
+import com.swiftyticket.models.Event;
+import com.swiftyticket.models.Role;
+import com.swiftyticket.models.User;
+import com.swiftyticket.models.Zones;
+import com.swiftyticket.repositories.EventRepository;
+import com.swiftyticket.repositories.UserRepository;
+import com.swiftyticket.repositories.ZoneRepository;
 
 @ContextConfiguration(classes = {ZoneServiceImpl.class})
 @ExtendWith(SpringExtension.class)
@@ -212,5 +209,101 @@ class ZoneServiceUnitTests {
     }
 
     // joinRaffle() and raffle() tests are remaining to be implemented in the future.
+    @Test
+    void joinRaffle_EventNotFound_ThrowException(){
+        //Arrange
+        Event event = mock(Event.class);
+        when(event.getEventId()).thenReturn(1);
+        // Event event = new Event();
+        // event.setDates(new ArrayList<>());
+        // event.setEventId(1);
+        // event.setEventName("Event 1");
+        // event.setOpen4Registration(true);
+        // EventRepository eventRepository = mock(EventRepository.class);
+        // Integer eventId = event.getEventId();
+
+        Zones zones = mock(Zones.class);
+        when(zones.getZoneId()).thenReturn(1);
+        // Zones zones = new Zones();
+        // zones.setEvent(event);
+        // zones.setTicketList(new ArrayList<>());
+        // zones.setZoneCapacity(1);
+        // zones.setZoneDate(new Date());
+        // zones.setZoneId(1);
+        // zones.setZoneName("Zone 1");
+        // Integer zoneId = zones.getZoneId();
+
+        // User user = mock(User.class);
+        // when(user.getUsername()).thenReturn("username1");
+        // User user = new User();
+        // user.setDateOfBirth(Date.from(LocalDate.of(1970, 1, 1).atStartOfDay().atZone(ZoneOffset.UTC).toInstant()));
+        // user.setEmail("test@gmail.com");
+        // user.setPassword("Iloveyou1!");
+        // user.setPhoneNumber("1234567890");
+        // user.setRole(Role.USER);
+        // user.setVerified(true);
+        // String username = user.getUsername();
+
+        //Act
+        when(jwtServiceImpl.extractUserName(Mockito.<String>any())).thenReturn("username1");
+
+        //Assert
+        assertThrows(EventNotFoundException.class, () -> zoneServiceImpl.joinRaffle("sjfnajdnad", event.getEventId(), zones.getZoneId()));
+
+        //to confirm that eventId is not found in eventRepository
+        Optional<Event> emptyValue = Optional.empty();
+        assertEquals(emptyValue, eventRepository.findById(event.getEventId()));
+
+        verify(jwtServiceImpl).extractUserName(Mockito.<String>any());
+    }
+
+    @Test
+    void joinRaffle_UserEmailNotFound_ThrowException() {
+        //Arrange
+        Event event = new Event();
+        event.setDates(new ArrayList<>());
+        event.setEventId(1);
+        event.setEventName("Event 1");
+        event.setOpen4Registration(true);
+        //if this is uncommented, it won't work for some reason
+        //EventRepository eventRepository = mock(EventRepository.class);
+        Integer eventId = event.getEventId();
+
+        Zones zones = new Zones();
+        zones.setEvent(event);
+        zones.setTicketList(new ArrayList<>());
+        zones.setZoneCapacity(1);
+        zones.setZoneDate(new Date());
+        zones.setZoneId(1);
+        zones.setZoneName("Zone 1");
+        Integer zoneId = zones.getZoneId();
+
+        User user = new User();
+        user.setDateOfBirth(Date.from(LocalDate.of(1970, 1, 1).atStartOfDay().atZone(ZoneOffset.UTC).toInstant()));
+        user.setEmail("test@gmail.com");
+        user.setPassword("Iloveyou1!");
+        user.setPhoneNumber("1234567890");
+        user.setRole(Role.USER);
+        user.setVerified(true);
+        String username = user.getUsername();
+        Integer userId = user.getUserId();
+
+        //Act
+        Optional<Event> ofResult = Optional.of(event);
+        when(eventRepository.findById(eventId)).thenReturn(ofResult);
+        when(jwtServiceImpl.extractUserName(Mockito.<String>any())).thenReturn(username);
+
+        //Assert
+        assertThrows(UserNotFoundException.class, () -> zoneServiceImpl.joinRaffle("sjfnajdnad", eventId, zoneId));
+
+        //to confirm that eventId is not found in eventRepository
+        Optional<User> emptyValue = Optional.empty();
+        assertEquals(emptyValue, userRepository.findById(userId));
+
+        verify(eventRepository).findById(eventId);
+        verify(jwtServiceImpl).extractUserName(Mockito.<String>any());
+    }
+
+
 }
 
